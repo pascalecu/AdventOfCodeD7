b6d3412820666e0c6ad7f42e004d6fbef75ea2e5
Ștefan-Iulian Alecu <165364995+pascalecu@users.noreply.github.com>
2026-07-01 02:20:34 +0300

    Refactor AoC.Registry with more useful functions

--- Core/AoC.Registry.pas
+++ Core/AoC.Registry.pas
@@ -9,6 +9,12 @@ procedure RegisterDay(Year, Day: Integer; C: TAoCDayClass);
 
 function GetDay(Year, Day: Integer): TAocDay;
 
+function IsRegistered(Year, Day: Integer): Boolean;
+
+function GetAvailableDayCount(Year: Integer): Integer;
+
+procedure GetAvailableDays(Year: Integer; var Days: array of Integer);
+
 implementation
 
 uses
@@ -23,45 +29,75 @@ const
 var
    Registry: array[MinYear..MaxYear, MinDay..MaxDay] of TAoCDayClass;
 
-procedure InitRegistry;
-var
-   Y, D: Integer;
+function ValidYear(Year: Integer): Boolean;
 begin
-   for Y := MinYear to MaxYear do
-      for D := MinDay to MaxDay do
-         Registry[Y, D] := nil;
+   Result := (Year >= MinYear) and (Year <= MaxYear);
+end;
 
+function ValidDay(Day: Integer): Boolean;
+begin
+   Result := (Day >= MinDay) and (Day <= MaxDay);
+end;
+
+function IsRegistered(Year, Day: Integer): Boolean;
+begin
+   Result := ValidYear(Year) and ValidDay(Day) and Assigned(Registry[Year, Day]);
 end;
 
 procedure RegisterDay(Year, Day: Integer; C: TAoCDayClass);
 begin
    if C = nil then
-      raise Exception.Create('RegisterDay: cannot register nil day class');
+      raise Exception.Create('RegisterDay: nil class');
 
-   if (Year < MinYear) or (Year > MaxYear) then
-      raise Exception.CreateFmt('RegisterDay: invalid year %d', [Year]);
+   if not ValidYear(Year) then
+      raise Exception.CreateFmt('invalid year %d', [Year]);
 
-   if (Day < MinDay) or (Day > MaxDay) then
-      raise Exception.CreateFmt('RegisterDay: invalid day: %d', [Day]);
+   if not ValidDay(Day) then
+      raise Exception.CreateFmt('invalid day: %d', [Day]);
 
    if Registry[Year, Day] <> nil then
-      raise Exception.CreateFmt('Day already registered: %d-%2.2d', [Year, Day]);
+      raise Exception.CreateFmt('Already registered: %d-%2.2d', [Year, Day]);
 
    Registry[Year, Day] := C;
 end;
 
 function GetDay(Year, Day: Integer): TAocDay;
 begin
-   if (Year < MinYear) or (Year > MaxYear) then
-      raise Exception.CreateFmt('GetDay: Invalid year %d', [Year]);
 
-   if (Day < MinDay) or (Day > MaxDay) then
-      raise Exception.CreateFmt('GetDay: Invalid day %d', [Day]);
-
-   if not Assigned(Registry[Year, Day]) then
-      raise Exception.CreateFmt('GetDay: Day %d-%2.2d not registered', [Year, Day]);
+   if not IsRegistered(Year, Day) then
+      raise Exception.CreateFmt('Day not registered %d-%2.2d', [Year, Day]);
 
    Result := Registry[Year, Day].Create;
+end;
+
+function GetAvailableDayCount(Year: Integer): Integer;
+var
+   D: Integer;
+begin
+   Result := 0;
+   if not ValidYear(Year) then
+      Exit;
+
+   for D := MinDay to MaxDay do
+      if Assigned(Registry[Year, D]) then
+         Inc(Result);
+end;
+
+procedure GetAvailableDays(Year: Integer; var Days: array of Integer);
+var
+   D, Index: Integer;
+begin
+   Index := 0;
+
+   if not ValidYear(Year) then
+      Exit;
+
+   for D := MinDay to MaxDay do
+      if (Assigned(Registry[Year, D])) and (Index <= High(Days)) then
+      begin
+         Days[Index] := D;
+         Inc(Index);
+      end;
 end;
 
 initialization
